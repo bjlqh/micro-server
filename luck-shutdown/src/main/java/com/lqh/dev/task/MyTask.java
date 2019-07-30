bedc1ba547cf9e5f5130ee470ae87441e4a201fc
liqihua5 <Lqh19920618>
2019-07-30 20:34:31 +0800

    创建线程池

--- luck-shutdown/src/main/java/com/lqh/dev/task/MyTask.java
+++ luck-shutdown/src/main/java/com/lqh/dev/task/MyTask.java
@@ -4,12 +4,43 @@ import lombok.extern.slf4j.Slf4j;
 import org.springframework.scheduling.annotation.Scheduled;
 import org.springframework.stereotype.Component;
 
+import java.util.Collection;
+import java.util.Iterator;
+import java.util.concurrent.BlockingDeque;
+import java.util.concurrent.BlockingQueue;
+import java.util.concurrent.ExecutorService;
+import java.util.concurrent.Executors;
+import java.util.concurrent.LinkedBlockingDeque;
+import java.util.concurrent.ThreadFactory;
+import java.util.concurrent.ThreadPoolExecutor;
+import java.util.concurrent.TimeUnit;
+import java.util.concurrent.atomic.AtomicInteger;
+
 @Slf4j
 @Component
 public class MyTask {
 
-    @Scheduled(fixedDelay = 3000)
-    public void taskExecutor() throws InterruptedException {
+    private static ExecutorService pool = Executors.newFixedThreadPool(5);
+
+    @Scheduled(fixedDelay = 30000)
+    public void executeTask() {
+        System.out.println("========任务开始=======");
+        for (int i = 1; i <= 20; i++) {
+            Work work = new Work("work " + i);
+            pool.execute(work);
+        }
+        pool.shutdown();
+        System.out.println("all thread work was finished");
+    }
+
+    @Scheduled(fixedDelay = 30000)
+    public void execute() {
+
+    }
+
+
+    //@Scheduled(fixedDelay = 3000)
+    public void task() throws InterruptedException {
         System.out.println("========任务开始=======");
         int count = 0;
         for (int i = 0; i < 20; i++) {
@@ -19,4 +50,5 @@ public class MyTask {
         }
         System.out.println("========任务结束：count=" + count);
     }
+
 }
